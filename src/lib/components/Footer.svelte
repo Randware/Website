39bbc7d421c4940a90848f5c609b50c8133e1621
GHaxZ <ghax.z.yt@proton.me>
2025-01-03 20:23:54 +0100

    Add node adapter, change layout

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
 	import FooterSection from './FooterSection.svelte';
 </script>
 
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,12 +7,13 @@
 </script>
 
 <div class="flex h-screen flex-col">
+	<!-- Add margins for Navbar -->
 	<header class="mx-6 mt-6">
 		<Navbar />
 	</header>
 
-	<!-- At margins for a uniform look -->
-	<main class="mx-12 my-32 flex-grow space-y-32 sm:mx-24 md:mx-48 lg:mx-36 xl:mx-48">
+	<!-- Add margins for a uniform look -->
+	<main class="mx-12 my-32 flex-grow sm:mx-24 md:mx-48 lg:mx-36 xl:mx-48">
 		{@render children()}
 	</main>
 
